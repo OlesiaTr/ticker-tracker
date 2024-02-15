c5098308385d0a4fa641e2e59ee73021f04e72ba
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-02-15 23:06:57 +0200

    * app.tsx

---
+++ client/src/pages/app/app.tsx
@@ -0,0 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Quote } from '../../components/quote';
+import { useTickerSubscription } from '../../service/socket';
+import { fetchTickersSuccess, selectLastTradeTime } from '../../slices/tickers';
+import { Chart } from '../../components/chart';
+import { useState } from 'react';
+import { THE_TICKER_ON_LOAD } from '../../constants';
+import { Loader } from '../../components/loader';
+import { StyledSelect } from '../../components/select-input';
+
+const App = () => {
+  const [selectedTicker, setSelectedTicker] =
+    useState<string>(THE_TICKER_ON_LOAD);
+  const [isLoading, setIsLoading] = useState(true);
+  const tickerTradeTime = useSelector(selectLastTradeTime(selectedTicker));
+  const dispatch = useDispatch();
+
+  useTickerSubscription(tickers => {
+    if (isLoading) {
+      dispatch(fetchTickersSuccess(tickers));
+      setIsLoading(false);
+    }
+  });
+
+  const handleTickerClick = (ticker: string) => setSelectedTicker(ticker);
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <main>
+      <h1 style={{ visibility: 'hidden' }}>Ticker Tracker</h1>
+      <h2 style={{ marginBottom: '20px', textAlign: 'center' }}>
+        Last time trade: {tickerTradeTime}
+      </h2>
+      <div style={{ maxWidth: '1200px', margin: '0 auto' }}>
+        <StyledSelect />
+
+        <Quote onTickerClick={handleTickerClick} />
+
+        <Chart selectedTicker={selectedTicker} />
+      </div>
+    </main>
+  );
+};
+
+export default App;
